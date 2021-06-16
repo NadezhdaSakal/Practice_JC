6297d89978d3c23802127f9f7f743e9fb4598764
NadezhdaSakal <79761587+NadezhdaSakal@users.noreply.github.com>
2021-06-16 20:06:38 +0300

    2 commit

---
+++ src/Skeleton.java
@@ -0,0 +1,6 @@
+public class Skeleton extends Character {
+
+    public Skeleton (String name, int HealthPoint, int Gold, int dexterity, int experience, int force) {
+        super(name, HealthPoint, Gold, dexterity, experience, force);
+    }
+}
